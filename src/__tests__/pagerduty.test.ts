b7f1a2dd3b79d436da0bb748615524b43b365ae7
Nick Dowmon <nick.dowmon@jupiterone.com>
2021-04-29 18:07:51 -0400

    Fix broken pagination in PagerDuty integration

--- src/__tests__/pagerduty.test.ts
+++ src/__tests__/pagerduty.test.ts
@@ -1,90 +1,46 @@
-import { mocked } from 'ts-jest/utils';
-import axios from 'axios';
-import range from 'lodash/range';
+import { requestAll, request } from '../pagerduty';
+import {
+  setupPagerDutyRecording,
+  Recording,
+} from '../../test/setupPagerDutyRecording';
+import { testConfig } from '../../test/config';
+import { User } from '../types';
 
-import { requestAll, request, restApi } from '../pagerduty';
+let recording: Recording;
 
-jest.mock('axios');
-const mockedAxios = mocked(axios, true);
-
-const token = 'api-token';
-
-beforeEach(() => {
-  jest.resetAllMocks();
+afterEach(async () => {
+  if (recording) {
+    await recording.stop();
+  }
 });
 
-describe('request', () => {
-  const successfulResponse = { data: { users: [] }, more: false };
-
-  test('returns response when request is successful', async () => {
-    const queryParams = { limit: 100 };
-    mockedAxios.get.mockResolvedValueOnce(successfulResponse);
-
-    await expect(request('/users', token, queryParams)).resolves.toEqual(
-      successfulResponse,
-    );
-    expect(mockedAxios.get).toBeCalledWith(`${restApi}/users`, {
-      headers: {
-        Authorization: `Token token=${token}`,
-        'Content-Type': 'application/json',
-      },
-      params: queryParams,
+describe('#request', () => {
+  test.only('should return limit and more properties', async () => {
+    recording = setupPagerDutyRecording({
+      directory: __dirname,
+      name: 'request::shouldReturnLimitAndMore',
     });
-  });
-
-  test.each([403, 429, 500])(
-    'retries the request when the request returns retryable status code: %d',
-    async (status) => {
-      const mockResponse = { data: { users: [] }, more: false };
-
-      mockedAxios.get
-        .mockRejectedValueOnce({ response: { status } })
-        .mockResolvedValueOnce(mockResponse);
-
-      await expect(request('/users', 'users')).resolves.toEqual(mockResponse);
-      expect(mockedAxios.get).toBeCalledTimes(2);
-    },
-  );
-
-  test.each([400, 401])(
-    'does not retry when the request returns non-retryable status code: %d',
-    async (status) => {
-      const mockResponse = { response: { status } };
-      mockedAxios.get.mockRejectedValueOnce(mockResponse);
-
-      await expect(request('/users', 'users')).rejects.toEqual(mockResponse);
-      expect(mockedAxios.get).toBeCalledTimes(1);
-    },
-  );
-
-  test('does not retry when the request returns an error without a status code', async () => {
-    const mockResponse = { response: { message: 'Network Error' } };
-    mockedAxios.get.mockRejectedValueOnce(mockResponse);
-
-    await expect(request('/users', 'users')).rejects.toEqual(mockResponse);
-    expect(mockedAxios.get).toBeCalledTimes(1);
+    const response = await request('/users', testConfig.apiKey);
+    expect(response.status).toBe(200);
+    expect(response.data.limit).toBe(25);
+    expect(response.data.more).toBe(false);
   });
 });
 
-describe('requestAll', () => {
-  test('returns paginate results within a single array', async () => {
-    const users = range(0, 10).map((id) => ({
-      id,
-    }));
+describe('#requestAll', () => {
+  test.only('should paginate response', async () => {
+    recording = setupPagerDutyRecording({
+      directory: __dirname,
+      name: 'requestAll::shouldPaginateResponse',
+    });
 
-    mockedAxios.get
-      .mockResolvedValueOnce({
-        data: { users: users.slice(0, 5) },
-        more: true,
-      })
-      .mockResolvedValueOnce({
-        data: { users: users.slice(5) },
-        more: false,
-      });
-
-    await expect(requestAll('/users', 'users', token, 5)).resolves.toEqual(
-      users,
+    const limitPerPage = 1;
+    const response = await requestAll<User>(
+      '/users',
+      'users',
+      testConfig.apiKey,
+      limitPerPage,
     );
-    expect(axios.get).toHaveBeenCalledTimes(2);
+    expect(response.length).toBeGreaterThan(limitPerPage);
   });
 });
